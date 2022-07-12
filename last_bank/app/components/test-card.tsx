ca8724a236a2eda489463821598a434079e71683
Derick <derickchoskinson@gmail.com>
2022-07-12 14:56:31 -0500

    even easier conditional render

--- last_bank/app/components/test-card.tsx
+++ last_bank/app/components/test-card.tsx
@@ -12,11 +12,14 @@ type Props = {
 export default function BillsCard ({ userBills }: Props) {
     const [show, setShow] = useState(false)
     const navigate = useNavigate()
+    const dataForDisplay = show ? userBills : userBills.slice(0, 4)
     let four = userBills.slice(0, 4)
     console.log(four)
-    if (show === false) {
-        return <>
-            { four.map((bill) => (
+
+    return (
+        <>
+            <div><button onClick={ () => setShow(!show) }>{ show ? 'show less' : 'show more' }</button></div>
+            { dataForDisplay.map((bill) => (
                 <div
                     className='flex flex-col items-center text-center text-base w-full bg-zinc-700 md:max-w-screen-xl rounded overflow-hidden shadow-2xl transition duration-500 ease-in-out delay-150 transform hover:-translate-y-1 hover:scale-110 m-2 py-2 md:text-lg'
                     key={ bill.id }
@@ -26,20 +29,5 @@ export default function BillsCard ({ userBills }: Props) {
             )) }
             <div><button onClick={ () => setShow(!show) }>{ show ? 'show less' : 'show more' }</button></div>
         </>
-    } else {
-        return (
-            <>
-                <div><button onClick={ () => setShow(!show) }>{ show ? 'show less' : 'show more' }</button></div>
-                { userBills.map((bill) => (
-                    <div
-                        className='flex flex-col items-center text-center text-base w-full bg-zinc-700 md:max-w-screen-xl rounded overflow-hidden shadow-2xl transition duration-500 ease-in-out delay-150 transform hover:-translate-y-1 hover:scale-110 m-2 py-2 md:text-lg'
-                        key={ bill.id }
-                    >
-                        <Card bill={ bill } />
-                    </div>
-                )) }
-                <div><button onClick={ () => setShow(!show) }>{ show ? 'show less' : 'show more' }</button></div>
-            </>
-        )
-    }
+    )
 }
