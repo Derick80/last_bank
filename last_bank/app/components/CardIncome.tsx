5133eec09be3a5c3ee8900e8c3bc4ef8e5ecbf72
Derick <derickchoskinson@gmail.com>
2022-07-20 02:19:31 -0500

    add bill working

--- last_bank/app/components/CardIncome.tsx
+++ last_bank/app/components/CardIncome.tsx
@@ -1,29 +1,29 @@
-import type { Income, Bill } from "@prisma/client";
-import { useNavigate } from "@remix-run/react";
-import { format } from "date-fns";
-import { useState } from "react";
-import { numberWithCommas } from "~/utils/format";
-import Tooltip from "./ToolTip";
+import type { Income, Bill } from "@prisma/client"
+import { useNavigate } from "@remix-run/react"
+import { format } from "date-fns"
+import { useState } from "react"
+import { numberWithCommas } from "~/utils/format"
+import Tooltip from "./shared/ToolTip"
 type Props = {
-  item: Income | Bill;
-  isBill: boolean;
-};
-export default function CardIncome({ item, isBill }: Props) {
-  const navigate = useNavigate();
-  const [expand, setExpand] = useState(false);
-  let ttMessage = !expand ? "Expand to See More" : "Collapse";
+  item: Income | Bill
+  isBill: boolean
+}
+export default function CardIncome ({ item, isBill }: Props) {
+  const navigate = useNavigate()
+  const [expand, setExpand] = useState(false)
+  let ttMessage = !expand ? "Expand to See More" : "Collapse"
   return (
     <div className="flex flex-row static w-full justify-between p-3">
       <div className="flex flex-col place-items-start">
-        <div>{item.source}</div>
-        <div>Due {format(new Date(item.due_date), "MMMM, do")}</div>
+        <div>{ item.source }</div>
+        <div>Due { format(new Date(item.due_date), "MMMM, do") }</div>
       </div>
       <div className="font-['Eczar'] text-base flex flex-row items-center md:text-xl">
-        ${numberWithCommas(item.amount)}
+        ${ numberWithCommas(item.amount) }
         <Tooltip message="Edit item">
           <div
             className="items-center p-2"
-            onClick={() =>
+            onClick={ () =>
               isBill
                 ? navigate(`bill/${item.id}`)
                 : navigate(`income/${item.id}`)
@@ -36,26 +36,26 @@ export default function CardIncome({ item, isBill }: Props) {
 
       <div
         className="text-center absolute bottom-0 left-1/2"
-        onClick={() => setExpand(!expand)}
+        onClick={ () => setExpand(!expand) }
       >
-        <Tooltip message={ttMessage}>
-          {expand ? (
+        <Tooltip message={ ttMessage }>
+          { expand ? (
             <span className="material-symbols-outlined">expand_less</span>
           ) : (
             <span className="material-symbols-outlined">expand_more</span>
-          )}
+          ) }
         </Tooltip>
       </div>
 
-      {expand === true ? (
+      { expand === true ? (
         <>
           <div className="flex flex-col place-items-start">
-            {" "}
-            <div>{item.paid}</div>
-            <div>{item.description}</div>
+            { " " }
+            <div>{ item.paid }</div>
+            <div>{ item.description }</div>
           </div>
         </>
-      ) : null}
+      ) : null }
     </div>
-  );
+  )
 }
